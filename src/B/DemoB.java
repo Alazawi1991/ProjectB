e0379120754ab46bd258bdd85a53b5775f5ff5ad
Alazawi1991 <155496191+Alazawi1991@users.noreply.github.com>
2024-01-03 21:50:29 -0500

    Update DemoB.java

--- src/B/DemoB.java
+++ src/B/DemoB.java
@@ -7,10 +7,13 @@ public class DemoB {
 		System.out.println("DemoB");
 		int s[]={1,2,3,4,5,6,7};
 		for (int a:s) {
+		System.out.println(a);
+
 			
-			
-			System.out.println(a);
 		}
+
+		public void int a (int c)
+			system.out.println(a);
 
 	}
 }
