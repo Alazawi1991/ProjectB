4f4bcd798fb3314a767d087d703c06c3e3274523
alaza <alaza@LAPTOP-MN9GES92>
2024-01-03 21:53:13 -0500

    method

--- src/B/DemoB.java
+++ src/B/DemoB.java
@@ -5,15 +5,17 @@ public class DemoB {
 	public static void main(String[] args) {
 
 		System.out.println("DemoB");
-		int s[]={1,2,3,4,5,6,7};
-		for (int a:s) {
-		System.out.println(a);
+		int s[] = { 1, 2, 3, 4, 5, 6, 7 };
+		for (int a : s) {
+			System.out.println(a);
 
-			
 		}
+	}
 
-		public void int a (int c)
-			system.out.println(a);
+	public static void method(int c) {
+
+		System.out.println(c);
 
 	}
+
 }
