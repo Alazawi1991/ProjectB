518b1d88ea3e63f6e532b23c8e2ef6e25cd6c367
alaza <alaza@LAPTOP-MN9GES92>
2024-01-03 21:48:58 -0500

    loop

--- src/B/DemoB.java
+++ src/B/DemoB.java
@@ -5,6 +5,12 @@ public class DemoB {
 	public static void main(String[] args) {
 
 		System.out.println("DemoB");
+		int s[]={1,2,3,4,5,6,7};
+		for (int a:s) {
+			
+			
+			System.out.println(a);
+		}
 
 	}
 }
